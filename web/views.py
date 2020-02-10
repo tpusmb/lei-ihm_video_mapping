236ba6d483bcc8113c9ee827fd15cc40146f8bec
sirambd <sirambd.tsone@gmail.com>
2020-02-10 22:04:26 +0100

    mapping projection tests

--- web/views.py
+++ web/views.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return "Hello world", 200
+
+
+@app.route('/test')
+def test():
+    return render_template('simple.html'), 200
 
 
 if __name__ == "__main__":
