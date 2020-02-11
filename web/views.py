df2732c74a0d815e5c147306e55cd950431dafbb
swan.blanc@etu.univ-smb.fr <magicswan1995>
2020-02-11 15:32:18 +0100

    add working version

--- web/views.py
+++ web/views.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request
 
+from py_video_mapping import PyVideoMapping, creat_monitor
+
 app = Flask(__name__)
+
+py_video_mapping = PyVideoMapping(PyVideoMapping.get_all_screens()[1])
+py_video_mapping.show_to_projector(py_video_mapping.wall_paper, blocking=False)
 
 
 @app.route('/')
@@ -15,7 +20,11 @@ def test():
 
 @app.route('/sendPoints', methods=['POST'])
 def send_points():
-    print(request.json)
+    print("update projector")
+    data_json = request.json
+    if py_video_mapping.screen_relation is None:
+        py_video_mapping.change_ui_screen(creat_monitor(data_json["width"], data_json["heigth"]))
+    py_video_mapping.mapping_calibration(data_json["points"])
     return "success", 200
 
 
