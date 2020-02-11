98d5567332d4216b1bcb3d181a2a4097f5c0b26d
sirambd <sirambd.tsone@gmail.com>
2020-02-11 13:56:06 +0100

    fix mapping data name

--- utils/save_mapping.py
+++ utils/save_mapping.py
@@ -1,14 +1,16 @@
 import json
 
+MAPPING_DATA = "mapping_data.txt"
+
 
 def save(data):
-    with open('data.txt', 'w') as outfile:
+    with open(MAPPING_DATA, 'w') as outfile:
         json.dump(data, outfile)
 
 
 def load():
     try:
-        with open('data.txt') as json_file:
+        with open(MAPPING_DATA) as json_file:
             return json.load(json_file)
     except FileNotFoundError:
         return None
