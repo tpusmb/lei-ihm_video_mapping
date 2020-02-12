13b940e600289880538557a417302d3abbabd102
swan.blanc@etu.univ-smb.fr <magicswan1995>
2020-02-12 16:36:20 +0100

    edit test

--- test_video_projection.py
+++ test_video_projection.py
@@ -1,7 +1,7 @@
 from py_video_mapping import *
 
 py_video_mapping = PyVideoMapping(PyVideoMapping.get_all_screens()[1])
-py_video_mapping.show_video(0, "dance.mp4")
+py_video_mapping.show_video(0, "ressources/videos/karaoke/dance.mp4")
 py_video_mapping.show_video(1, "ressources/videos/karaoke/karaoke_gang-namseutayil.mp4")
 py_video_mapping.show_video(2, "ressources/videos/karaoke/dance.mp4")
 input("enter top stop")
