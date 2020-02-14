8121c18be714aff8668ca15e651f389bc37e81dc
sirambd <sirambd.tsone@gmail.com>
2020-02-14 14:01:59 +0100

    set flower rank

--- datas/models/Flower.py
+++ datas/models/Flower.py
@@ -10,9 +10,22 @@ class Mood(Enum):
 
 
 class Flower:
+    MIN_LEVEL = 1
+    MAX_LEVEL = 5
 
-    def __init__(self, level, mood: Mood, planted_at=date.today()):
+    def __init__(self, mood: Mood, rank: int = 1, planted_at=date.today()):
         self.mood = mood  # based on the last time the plant was looked after
-        self.level = level
+        self.rank = rank
         self.planted_at = planted_at
         self.updated_at = date.today()
+
+    @property
+    def rank(self):
+        return self.rank
+
+    @rank.setter
+    def rank(self, value):
+        if self.MIN_LEVEL <= value <= self.MAX_LEVEL:
+            self.rank = value
+        else:
+            raise ValueError
