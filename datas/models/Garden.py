f65c02f9608a5988d80f88bab4c90effc9b554e8
Basma <aly-nayef.basma@soprasteria.com>
2020-03-04 13:33:29 +0100

    temperature et humidité

--- datas/models/Garden.py
+++ datas/models/Garden.py
@@ -1,9 +1,15 @@
 from datas.models.Flower import Flower
+from parrot_sensor.captor_data import CaptorData
 
 
 class Garden:
 
     def __init__(self, flower: Flower):
         self.flower = flower
-        self.temperature = 0.0
-        self.humidity = 0.0
+        self.captor_data = CaptorData()
+        self.temperature = 0
+        self.humidity = 0
+        self.refresh_data()
+
+    def refresh_data(self):
+        self.temperature, self.humidity = self.captor_data.get_sensor_data()
