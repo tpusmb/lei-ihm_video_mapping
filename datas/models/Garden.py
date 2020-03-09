d27efe0288e55b16d52fa957b8819d3e23587adc
sirambd <sirambd.tsone@gmail.com>
2020-03-09 11:54:18 +0100

    Garden docstring

--- datas/models/Garden.py
+++ datas/models/Garden.py
@@ -4,6 +4,10 @@ from utils.config_reader import ConfigReader
 
 
 class Garden:
+    """
+    Garden Model, which represents the garden
+    We can have lots of information about the garden, such as the temperature and humidity of the garden
+    """
 
     def __init__(self, flower: Flower, config_reader: ConfigReader = ConfigReader()):
         self.flower = flower
@@ -13,4 +17,7 @@ class Garden:
         self.refresh_data()
 
     def refresh_data(self):
+        """
+        Update garden and humidity from the captor data
+        """
         self.temperature, self.humidity = self.captor_data.get_sensor_data()
