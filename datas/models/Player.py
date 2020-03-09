309b79bd1938dfa10dc3faa29885de19607f7f32
sirambd <sirambd.tsone@gmail.com>
2020-03-09 11:49:14 +0100

    Player docstring

--- datas/models/Player.py
+++ datas/models/Player.py
@@ -1,6 +1,7 @@
 class Player:
-    LEVEL_1 = 30
-    STEP_LEVEL = 10
+    """Player Model, which represents the user of our system"""
+    MIN_XP_LEVEL_1 = 30
+    STEP_LEVEL = 10  # used to calculate xp necessary needed for each level
 
     def __init__(self, name: str = "LEI"):
         self.name = name
@@ -11,12 +12,16 @@ class Player:
     def xp(self): return self.__xp
 
     @xp.setter
-    def xp(self, value):
+    def xp(self, value: int):
+        """
+        Set the player xp, and raise the level of the player when the XP is satisfied
+        :param value: The xp value
+        :return: player's xp
+        """
         self.__xp = value
         if self.xp >= self.total_xp_needed_for_next_level():
             self.level += 1
             self.__xp = 0
 
     def total_xp_needed_for_next_level(self):
-        return self.LEVEL_1 + self.STEP_LEVEL * self.level
-
+        return self.MIN_XP_LEVEL_1 + self.STEP_LEVEL * self.level
