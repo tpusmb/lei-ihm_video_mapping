ac8161d6367b1fcd4b243db96b61975d088feed9
sirambd <sirambd.tsone@gmail.com>
2020-03-09 13:37:31 +0100

    FlowerRepository.py docstring

--- datas/repositories/FlowerRepository.py
+++ datas/repositories/FlowerRepository.py
@@ -2,11 +2,17 @@ from datas.models.Flower import Flower
 
 
 class FlowerRepository:
+    """
+    Manage the flower
+    """
     CHANGE_FLOWER_RANK_MINUTE = 5  # Minute
 
     def __init__(self, flower: Flower):
         self.flower = flower
 
     def change_rank(self):
+        """
+        Increment the rank of the plant, simply when the limit has not been reached
+        """
         if not self.flower.is_last_rank():
             self.flower.rank += 1
