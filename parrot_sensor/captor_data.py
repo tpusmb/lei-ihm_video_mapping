3491098f71536d431be5e54b5871193d6078d956
swan.blanc@etu.univ-smb.fr <magicswan1995>
2020-03-11 17:09:40 +0100

    put offline sensor

--- parrot_sensor/captor_data.py
+++ parrot_sensor/captor_data.py
@@ -25,41 +25,43 @@ class CaptorData:
         self.since = (datetime.datetime.now() - datetime.timedelta(hours=time_delta)).strftime("%d-%b-%Y %H:%M:%S")
         self.today = datetime.datetime.now().strftime("%d-%b-%Y %H:%M:%S")
         self.config = self.get_projetconfig(config_reader)
-        self.connection = api_cloud.ApiCloud(self.config[0], self.config[1])
+        self.user_id = config_reader.Parrot["userid"]
+        self.user_code = config_reader.Parrot["usercode"]
+        self.password = config_reader.Parrot["passwd"]
+        self.have_sensor = self.user_id is not None and self.user_code is not None and self.password is not None
+        if self.have_sensor:
+            self.connection = api_cloud.ApiCloud(self.user_id, self.user_code)
+        else:
+            self.connection = None
 
     def get_sensor_data(self):
         """
         retrieves data from sensor
         :return:
         """
-        self.connection = api_cloud.ApiCloud(self.config[0], self.config[1])
-        self.connection.login(self.config[0], self.config[2])
-        location_identifier = self.get_identifier_location()
-        return self.init_temperature_and_humidity(self.connection.get_samples_location(location_identifier, self.since,
-                                                                                       self.today))
+        if self.have_sensor:
+            self.connection = api_cloud.ApiCloud(self.user_id, self.user_code)
+            self.connection.login(self.user_id, self.password)
+            location_identifier = self.get_identifier_location()
+            return self.init_temperature_and_humidity(
+                self.connection.get_samples_location(location_identifier, self.since, self.today))
+        else:
+            return 0, 0
 
     def get_identifier_location(self):
         """
         recover the position of the plant
         :return:
         """
-        parrot_data = json.loads(
-            json.dumps(self.connection.get_sensor_data_sync()))["locations"][0]["location_identifier"]
-        return parrot_data
+        if self.have_sensor:
+            parrot_data = json.loads(
+                json.dumps(self.connection.get_sensor_data_sync()))["locations"][0]["location_identifier"]
+            return parrot_data
+        else:
+            return {}
 
-    def get_projetconfig(self, config_reader):
-        """
-        initialize the configuration from a json file
-        :param config_reader: (ConfigReader) config class
-        :return: liste avec les infos de connexion
-        """
-        config_data = list()
-        config_data.append(config_reader.Parrot["userid"])
-        config_data.append(config_reader.Parrot["usercode"])
-        config_data.append(config_reader.Parrot["passwd"])
-        return config_data
-
-    def init_temperature_and_humidity(self, filename):
+    @staticmethod
+    def init_temperature_and_humidity(filename):
         """
         get temperature and humidity from sensor data
         :param filename: fichier json
